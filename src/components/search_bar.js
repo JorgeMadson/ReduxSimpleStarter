970e8b5f0f33626f27f2770f45f63b82ab0ab3d4
Jorge Madson <filholouco@gmail.com>
2018-01-09 16:40:21 -0300

    Pesquisa funcionando

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,13 +13,17 @@ class SearchBar  extends Component {
       //Aqui tenho uma função recebendo o event como parametro e
       //modificando o valor do state quando o evento onChange acontece
 //Isso é um contolled component
-      <div>
+      <div className="search-bar">
         <input
         value  = {this.state.term}
-        onChange={event => this.setState({ term:  event.target.value })} />
+        onChange={event => this.onInputChange(event.target.value)} />
       </div>
       );
+  }
 
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -19,19 +19,22 @@ class App extends Component {
     selectedVideo: null
   };
 
-  YTSearch({key: API_KEY, term: 'charlie brown jr'}, (videos) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
+  this.videoSearch('charlie brown jr');
+}
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
       });
-      // this.setState({videos: videos})
-    });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
